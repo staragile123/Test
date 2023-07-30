f6b5a28d226361ae7b0e438ed3dbd2be804bc9bb
Jeevitha771 <jeevithayadavkypa@gmail.com>
2023-07-30 15:37:26 +0530

    hello feature added

--- hello.java
+++ hello.java
@@ -4,5 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("Hello Jeevitha");
+	System.out.println("Hello feature/devops!");
+
     }
 }
